6db58179002f211c773e2a0e536ffae54c0e5bd8
Sam Dotson <samgdotson@gmail.com>
2026-08-18 09:07:47 -0500

    updates the isone api to hourlysysload (Load,NativeLoad,ArdDemand)

--- workflow/scripts/retrieve_isone_rt_demand.py
+++ workflow/scripts/retrieve_isone_rt_demand.py
@@ -5,36 +5,31 @@ if __name__=="__main__":
     from tqdm import tqdm
 
     BASE_URL = "https://webservices.iso-ne.com/api/v1.1"
-    market = "realtimehourlydemand"
+    market = 'hourlysysload'
     fmt = ".json"
     dates = pd.date_range(snakemake.config['start_date'],snakemake.config['end_date'], freq='D')
-    location_ids = list(range(4001,4009))
+    # location_ids = list(range(4001,4009))
 
     auth = (
             snakemake.params.username, 
             snakemake.params.password
             )
-    full_frames = []
+    frames = []
     for d in tqdm(dates):
-        frames = []
-        for loc in location_ids:
-            url = "/".join([BASE_URL, 
-                            market, 
-                            'day', 
-                            d.strftime("%Y%m%d"), 
-                            'location', 
-                            str(loc)]) + fmt
-            r = requests.get(url, auth=auth, headers={"Accept":"application/json"})
-            if r.status_code != 200:
-                print(f"Status Code: {r.status_code} for url: {url}")
-            data = r.json()['HourlyRtDemands']['HourlyRtDemand']
-            df = pd.DataFrame(data)
-            df['BeginDate'] = pd.to_datetime(df['BeginDate'], utc=False)
-            df = df[['BeginDate', 'Load']]
-            df.columns = ['datetime',loc]
-            df.set_index('datetime', inplace=True)
-            frames.append(df)
-        full_frames.append(pd.concat(frames, axis=1))
+        url = "/".join([BASE_URL, 
+                        market, 
+                        'day', 
+                        d.strftime("%Y%m%d"), 
+                        ]
+                        ) + fmt
+        r = requests.get(url, auth=auth, headers={"Accept":"application/json"})
+        if r.status_code != 200:
+            print(f"Status Code: {r.status_code} for url: {url}")
+        df = pd.DataFrame(r.json()['HourlySystemLoads']['HourlySystemLoad'])
+        df['BeginDate'] = pd.to_datetime(df['BeginDate'], utc=False)
+        df = df[['BeginDate', 'Load','NativeLoad','ArdDemand']]
+        df.set_index('BeginDate', inplace=True)
+        frames.append(df)
 
-    full_df = pd.concat(full_frames).sum()
+    full_df = pd.concat(full_frames)
     full_df.to_csv(snakemake.output.demand_data)
